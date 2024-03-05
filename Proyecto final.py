18cbf548ff78cdfec275779231b872d56e0ea579
JKar-Gatgens <159174413+JKar-Gatgens@users.noreply.github.com>
2024-03-05 11:43:42 -0600

    Update Proyecto final.py

--- Proyecto final.py
+++ Proyecto final.py
@@ -1,8 +1,1 @@
-# tabla de multiplicar
-for i in range (1,13):
-    for j in range(1,13):
-        for k in range(1,13):
-            print(i, "x", j, "x", k, "=", i*j*k)
-        print("***********Fin for k****")
-    print("***********Fin for j****")  
-print("***********Fin for i****")             
+
